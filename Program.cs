9b3e770de3b09d0f0b92d9164ab9674cd84ffef9
leticialbuk <leticia.albuk@gmail.com>
2023-03-31 16:08:57 -0300

    base do projeto

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProtocoloAvc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Abrindo protocolo AVC");
+            Console.WriteLine("---------------------");
+
+            Console.Write("Nome do paciente: ");
+            Console.ReadLine();
+
+            Console.Write("Idade: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Número do protcolo: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Iniciando protocolo AVC.....");
+            Console.WriteLine("AVISO: Este protocolo deverá ser feito em 1 hora, risco iminente de morte!");
+
+            //Console.WriteLine("Paciente foi encaminhado a sala de emergência? ");
+        }
+
+
+    }
+}
