e028ad28dd11c9b7de8683624052f5e91e276168
leticialbuk <leticia.albuk@gmail.com>
2023-04-02 21:56:22 -0300

    utilizando if/else no protocolo de avc

--- Program.cs
+++ Program.cs
@@ -22,10 +22,54 @@ namespace ProtocoloAvc
             Console.WriteLine("---------------------");
             Console.WriteLine("Iniciando protocolo AVC.....");
             Console.WriteLine("AVISO: Este protocolo deverá ser feito em 1 hora, risco iminente de morte!");
+            Console.WriteLine("--------------------------------------------------------------------------");
 
-            //Console.WriteLine("Paciente foi encaminhado a sala de emergência? ");
+            string entradaEmergencia;
+            do
+            {
+                Console.WriteLine("O paciente deu entrada na emergência? sim ou nao");
+                entradaEmergencia = Console.ReadLine();
+
+                if (entradaEmergencia.ToLower() == "sim")
+                {
+                    Console.WriteLine("Daremos continuidade ao atendimento...");
+                    ContinuidadeAtendimentoEtapa1();
+                }
+                else if (entradaEmergencia.ToLower() == "nao")
+                {
+                    Console.WriteLine("O paciente deve ser encaminhado a emergência!");
+                }
+                else
+                {
+                    Console.WriteLine("Resposta inválida");
+                }
+            } while (entradaEmergencia.ToLower() != "sim" && entradaEmergencia.ToLower() != "nao");
+
         }
 
+        public static void ContinuidadeAtendimentoEtapa1()
+        {
+            string acessoVenoso;
+            do
+            {
+                Console.WriteLine("Foi pulsionado acesso venoso no paciente? sim ou nao");
+                acessoVenoso = Console.ReadLine();
+
+                if (acessoVenoso.ToLower() == "sim")
+                {
+                    Console.WriteLine("Daremos continuidade ao atendimento...");
+                    // ContinuidadeAtendimentoEtapa2();
+                }
+                else if (acessoVenoso.ToLower() == "nao")
+                {
+                    Console.WriteLine("Deve ser feito acesso venoso no paciente para darmos continuidade ao aendimento!");
+                }
+                else
+                {
+                    Console.WriteLine("Resposta inválida");
+                }
+            } while (acessoVenoso.ToLower() != "sim" && acessoVenoso.ToLower() != "nao");
+        }
 
     }
 }
